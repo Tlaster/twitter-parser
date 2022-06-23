5730d7be4d23d0b9772028ac44336487fe3ad345
Tlaster <tlaster@outlook.com>
2022-06-23 12:10:26 +0800

    add maven publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     kotlin("multiplatform") version "1.7.0"
+    id("maven-publish")
+    id("signing")
 }
 
 group = "moe.tlaster"
-version = "1.0-SNAPSHOT"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
@@ -49,5 +51,61 @@ kotlin {
         val jsTest by getting
         val nativeMain by getting
         val nativeTest by getting
+    }
+}
+
+
+val javadocJar: TaskProvider<Jar> by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+}
+
+publishing {
+    if (rootProject.file("publish.properties").exists()) {
+        signing {
+            sign(publishing.publications)
+        }
+        repositories {
+            maven {
+                val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+                val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+                url = if (version.toString().endsWith("SNAPSHOT")) {
+                    uri(snapshotsRepoUrl)
+                } else {
+                    uri(releasesRepoUrl)
+                }
+                credentials {
+                    username = project.ext.get("ossrhUsername").toString()
+                    password = project.ext.get("ossrhPassword").toString()
+                }
+            }
+        }
+    }
+
+    publications.withType<MavenPublication> {
+        artifact(javadocJar)
+        pom {
+            name.set("twitter-parser")
+            description.set("Twitter parser")
+            url.set("https://github.com/Tlaster/twitter-parser")
+
+            licenses {
+                license {
+                    name.set("MIT")
+                    url.set("https://opensource.org/licenses/MIT")
+                }
+            }
+            developers {
+                developer {
+                    id.set("Tlaster")
+                    name.set("James Tlaster")
+                    email.set("tlaster@outlook.com")
+                }
+            }
+            scm {
+                url.set("https://github.com/Tlaster/twitter-parser")
+                connection.set("scm:git:git://github.com/Tlaster/twitter-parser.git")
+                developerConnection.set("scm:git:git://github.com/Tlaster/twitter-parser.git")
+            }
+        }
     }
 }
